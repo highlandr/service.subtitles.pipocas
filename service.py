ede7f9f9ff7509d5613a8d52ebd13f5ff37f7808
Connor McLeod <highlander@teknorage.com>
2014-06-17 23:37:29 +0100

    Update service.py
    New minor version.

--- service.py
+++ service.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Service Pipocas.tv version 0.1.0
+# Service Pipocas.tv version 0.1.1
 # Code based on Undertext (FRODO) service
 # Coded by HiGhLaNdR@OLDSCHOOL
 # Ported to Gotham by HiGhLaNdR@OLDSCHOOL
